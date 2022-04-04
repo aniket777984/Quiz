1625bb2ae776f4ceb5498cbaf3aa1f507f99a528
aniket777984 <aniket777984@gmail.com>
2022-04-04 22:37:53 +0530

    Newly made a quiz app using react

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <div style={{textAlign:"center" , marginBottom : 10}}>
+        Made with ♥ by {" "}
+        <a
+        href="https://www.youtube.com/roadsidecode"
+        style={{ cursor: "pointer" }}
+      >
+        Aniket Kumar Gupta
+      </a> 
+    </div>
+  )
+}
+
+export default Footer
